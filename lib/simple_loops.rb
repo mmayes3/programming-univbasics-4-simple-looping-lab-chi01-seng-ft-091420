da65a78c8a3b88bfad5c3bc4f9019de808c1c3f6
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-29 17:24:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,4 +6,12 @@ def loop_message_five_times(string)
   end
 end
 
-loop_message_five_times("h")
+def loop_message_n_times(string, integer)
+  count = 0
+  while count < integer do
+    puts string
+    count += 1
+  end
+end
+
+loop_message_n_times("yup", 5)
